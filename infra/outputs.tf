b6383bf9d65347ba14c9d87d6fec596982e981ec
mohamed amine abdrrazek <mohamed-amine.abdrrazek@talan.com>
2026-05-24 00:30:26 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -55,7 +55,20 @@ output "glue_test_script_arn" {
 }
 
 output "glue_test_script_etag" {
-  value = aws_s3_object.glue_test_script.etag
+  value     = aws_s3_object.glue_test_script.etag
+  sensitive = true
+}
+
+output "glue_test_script_2_s3_uri" {
+  value = "s3://${aws_s3_bucket.glue_output.bucket}/${aws_s3_object.glue_test_script_2.key}"
+}
+
+output "glue_test_script_2_arn" {
+  value = aws_s3_object.glue_test_script_2.arn
+}
+
+output "glue_test_script_2_etag" {
+  value     = aws_s3_object.glue_test_script_2.etag
   sensitive = true
 }
 
@@ -77,4 +90,12 @@ output "glue_job_name" {
 
 output "glue_job_arn" {
   value = aws_glue_job.count_job.arn
+}
+
+output "glue_job_2_name" {
+  value = aws_glue_job.count_job_2.id
+}
+
+output "glue_job_2_arn" {
+  value = aws_glue_job.count_job_2.arn
 }
